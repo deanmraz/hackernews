edaf47686bb03de0608cb4b80a3918a98269fa50
Dean Mraz <deanm@runtriz.com>
2016-04-16 14:26:53 -0700

    extract domain helper

---
+++ app/helpers/extract-domain.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export function extractDomain(params) {
+  let url = params[0];
+  if(Ember.isEmpty(url)) return "";
+  let domain;
+  //find & remove protocol (http, ftp, etc.) and get domain
+  if (url.indexOf("://") > -1) {
+    domain = url.split('/')[2];
+  }
+  else {
+    debugger;
+    domain = url.split('/')[0];
+  }
+  //find & remove port number
+  domain = domain.split(':')[0];
+
+  // remove www.
+  if(domain.search('www.') !== -1) {
+    domain = domain.substring(4,domain.length);
+  }
+
+  return domain.toLowerCase();
+}
+
+export default Ember.Helper.helper(extractDomain);
---
+++ tests/unit/helpers/extract-domain-test.js
@@ -0,0 +1,10 @@
+import { extractDomain } from 'hackernews/helpers/extract-domain';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | extract domain');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = extractDomain([42]);
+  assert.ok(result);
+});
